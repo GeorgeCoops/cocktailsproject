4b83907b00121ea8da01cf9e7285fa6913b138c2
BenDunjay <bendunjay@outlook.com>
2020-04-10 13:03:50 +0100

    All validates done pt.2

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -10,9 +10,8 @@ class Cocktail < ApplicationRecord
 
   validates :name, presence: true 
   validates :name, length: { maximum: 20 }
-  validates :colour, format: { with: /\A[a-zA-Z]+\z/,
-  message: "only allows letters" }
-  validates :sweet_sour, presence: true 
-  validates :sweet_sour, inclusion: { in: %w(sweet sour), 
+  validates :difficulty, inclusion: { in: %w(easy medium hard), 
   message: "%{value} is not a valid choice."}
+  validates :calories, inclusion: { in: %w(0..1000)}
+
 end
