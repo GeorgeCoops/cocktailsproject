350566ec00b51ef1d3b183020d1a66ec74fa35d8
BenDunjay <bendunjay@outlook.com>
2020-04-10 12:59:54 +0100

    Validations done

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -8,5 +8,11 @@ class Cocktail < ApplicationRecord
   has_many :garnishes, through: :cocktail_garnishes
   has_many :mixers, through: :cocktail_mixers
 
-  
+  validates :name, presence: true 
+  validates :name, length: { maximum: 20 }
+  validates :colour, format: { with: /\A[a-zA-Z]+\z/,
+  message: "only allows letters" }
+  validates :sweet_sour, presence: true 
+  validates :sweet_sour, inclusion: { in: %w(sweet sour), 
+  message: "%{value} is not a valid choice."}
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
     has_many :mixers, through: :cocktails
     has_many :garnishes, through: :cocktails
     has_many :alcohols, through: :cocktails
+
+    validates :name, presence: true 
+    validates :name, length: { maximum: 20 }
+    validates :name, format: { with: /\A[a-zA-Z]+\z/,
+    message: "only allows letters" }
+    validates :location, length: { maximum: 20 }
+    validates :location, format: { with: /\A[a-zA-Z]+\z/,
+    message: "only allows letters" }
+    validates :age, numericality: { greater_than_or_equal_to: 18}
 end
+
+
